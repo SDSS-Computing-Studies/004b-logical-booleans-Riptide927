217bb002012541343f32d78c85414d51342a99a3
Peter <PeterC499@deltalearns.ca>
2020-10-02 11:20:01 -0700

    ggg

--- task2.py
+++ task2.py
@@ -20,13 +20,13 @@ Enter a number: 8
 """
 import math
 
-n= int(input("Give me a number "))
+n= float(input("Give me a number "))
 s= math.sqrt(n)
-c = (n**1/3)
-
+c = round(n**(1/3),1)
+n = int(n)
 if c%1 == 0 and s%1 == 0:
     print(n,"is both a perfect square and a perfect cube.")
 elif s%1 ==0:
     print(n,"is only a perfect square.")
-elif c%1 == 0:
+elif c %1==0:
     print(n,"is only a perfect cube.")
